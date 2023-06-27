303cb71a05599affa8ad95ccf8afb6943ebe3d1a
Adam Cross <adamfgcross@gmail.com>
2023-06-27 19:28:38 -0400

    fix some problems in 1.2 and add new syntax easier to read

--- src/data-is.test.ts
+++ src/data-is.test.ts
@@ -9,24 +9,45 @@ describe("define.type([typeString], [evaluationFunction])", () => {
         }).not.toThrow();
     });
 });
-describe("element([data]).is([type])", () => {
+describe("element([data]).is.a([type])", () => {
     it("returns true on valid data type", () => {
         const data = DataIs.build();
         data.define.type("number", (x) => typeof x == "number");
         const testVal = 5;
-        expect(data.element(testVal).is("number")).toBeTruthy();
+        expect(data.element(testVal).is.a("number")).toBeTruthy();
     });
     it("returns false on invalid data type", () => {
         const data = DataIs.build();
         data.define.type("number", (x) => typeof x == "number");
         const testVal = "string";
-        expect(data.element(testVal).is("number")).toBeFalsy();
+        expect(data.element(testVal).is.a("number")).toBeFalsy();
     });
     it("throws if data type is not defined", () => {
         const data = DataIs.build();
         const testVal = "string";
         expect(() => {
-            data.element(testVal).is("number");
+            data.element(testVal).is.a("number");
+        }).toThrow();
+    });
+});
+describe("element([data]).is.an([type])", () => {
+    it("returns true on valid data type", () => {
+        const data = DataIs.build();
+        data.define.type("number", (x) => typeof x == "number");
+        const testVal = 5;
+        expect(data.element(testVal).is.an("number")).toBeTruthy();
+    });
+    it("returns false on invalid data type", () => {
+        const data = DataIs.build();
+        data.define.type("number", (x) => typeof x == "number");
+        const testVal = "string";
+        expect(data.element(testVal).is.an("number")).toBeFalsy();
+    });
+    it("throws if data type is not defined", () => {
+        const data = DataIs.build();
+        const testVal = "string";
+        expect(() => {
+            data.element(testVal).is.an("number");
         }).toThrow();
     });
 });
@@ -35,19 +56,63 @@ describe("notation data([element]).is([typeString])", () => {
         const data = DataIs.build();
         data.define.type("number", (x) => typeof x == "number");
         const testVal = 5;
-        expect(data(testVal).is("number")).toBeTruthy();
+        expect(data(testVal).is.a("number")).toBeTruthy();
     });
     it("returns false on invalid data type", () => {
         const data = DataIs.build();
         data.define.type("number", (x) => typeof x == "number");
         const testVal = "string";
-        expect(data(testVal).is("number")).toBeFalsy();
+        expect(data(testVal).is.a("number")).toBeFalsy();
     });
     it("throws if data type is not defined", () => {
         const data = DataIs.build();
         const testVal = "string";
         expect(() => {
-            data(testVal).is("number");
+            data(testVal).is.a("number");
         }).toThrow();
+    });
+});
+describe("using old is([typeString]) notation", () => {
+    describe("element([data]).is([type])", () => {
+        it("returns true on valid data type", () => {
+            const data = DataIs.build();
+            data.define.type("number", (x) => typeof x == "number");
+            const testVal = 5;
+            expect(data.element(testVal).is("number")).toBeTruthy();
+        });
+        it("returns false on invalid data type", () => {
+            const data = DataIs.build();
+            data.define.type("number", (x) => typeof x == "number");
+            const testVal = "string";
+            expect(data.element(testVal).is("number")).toBeFalsy();
+        });
+        it("throws if data type is not defined", () => {
+            const data = DataIs.build();
+            const testVal = "string";
+            expect(() => {
+                data.element(testVal).is("number");
+            }).toThrow();
+        });
+    });
+    describe("notation data([element]).is([typeString])", () => {
+        it("returns true on valid data type", () => {
+            const data = DataIs.build();
+            data.define.type("number", (x) => typeof x == "number");
+            const testVal = 5;
+            expect(data(testVal).is("number")).toBeTruthy();
+        });
+        it("returns false on invalid data type", () => {
+            const data = DataIs.build();
+            data.define.type("number", (x) => typeof x == "number");
+            const testVal = "string";
+            expect(data(testVal).is("number")).toBeFalsy();
+        });
+        it("throws if data type is not defined", () => {
+            const data = DataIs.build();
+            const testVal = "string";
+            expect(() => {
+                data(testVal).is("number");
+            }).toThrow();
+        });
     });
 });
--- src/data-is.ts
+++ src/data-is.ts
@@ -5,19 +5,29 @@ type verifyFunction = (x:any) => boolean;
 const UndefinedTypeException = new Error("we-assert: undefined type");
 
 
+
+
 export default {
     build : function () {
+
+        const $isA = (element:any, dataTypeString:string) : boolean => {
+            if (types[dataTypeString]) {
+                return types[dataTypeString](element);
+            } else {
+                throw UndefinedTypeException;
+            }
+        };
+
+
+
         const types:DataTypes = {};
-        const data = (element?:any) => {
-            return {
-                is (dataTypeString:string) : boolean {
-                    if (types[dataTypeString]) {
-                        return types[dataTypeString](element);
-                    } else {
-                        throw UndefinedTypeException;
-                    }
-                }
+        const data = (element:any) => {
+            const is = (dataTypeString:string) => {
+                return $isA(element, dataTypeString);
             };
+            is.a = (dataTypeString:string) => $isA(element, dataTypeString);
+            is.an = (dataTypeString:string) => $isA(element, dataTypeString);
+            return {is};
         };
         data.define = {
             type : function (typeName:string, vEval :verifyFunction ) :void {
@@ -25,15 +35,12 @@ export default {
             }
         };
         data.element = (element: any) => {
-            return {
-                is (dataTypeString:string) : boolean {
-                    if (types[dataTypeString]) {
-                        return types[dataTypeString](element);
-                    } else {
-                        throw UndefinedTypeException;
-                    }
-                }
+            const is = (dataTypeString:string) => {
+                return $isA(element, dataTypeString);
             };
+            is.a = (dataTypeString:string) => $isA(element, dataTypeString);
+            is.an = (dataTypeString:string) => $isA(element, dataTypeString);
+            return {is};
         };
         return data;
     }
